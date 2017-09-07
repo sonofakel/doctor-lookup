497aa0f794f90c038447242b98804683cf5390bb
Charlie Kelson <charliek1986@gmail.com>
2017-09-07 11:16:39 -0700

    add spec folder for js hint to search

--- gulpfile.js
+++ gulpfile.js
@@ -59,7 +59,7 @@ gulp.task("clean", function(){
 });
 
 gulp.task('jshint', function(){
-  return gulp.src(['js/*.js'])
+  return gulp.src(['js/*.js','spec/*.js'])
   .pipe(jshint())
   .pipe(jshint.reporter('default'));
 });
