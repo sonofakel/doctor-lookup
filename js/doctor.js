a1828d28e51cd8f891d4218b4c603c97d52ffefd
Charlie Kelson <charlie.m.kelson@gmail.com>
2017-09-15 08:21:03 -0700

    initial commit.

---
+++ js/doctor.js
@@ -0,0 +1,17 @@
+export class Doctor {
+constructor () {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.address = address;
+  this.phone = phone;
+  this.website = website;
+  this.accepting = accepting;
+
+findMedIssue(medIssue) {
+
+}
+
+findDoctorByName(name) {
+
+}
+};
