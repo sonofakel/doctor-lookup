2e4f0ea69eae6f82ede76eecec1705e7dec09bac
Charlie Kelson <charlie.m.kelson@gmail.com>
2017-09-15 12:11:19 -0700

    finish styling

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${medIssue}&location=47.6129432%2C-122.4821474%2C100&user_location=47.6129432%2C-122.4821474&skip=0&limit=25&user_key=${apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${medIssue}&location=47.6129432%2C-122.4821474%2C25&user_location=47.6129432%2C-122.4821474&skip=0&limit=25&user_key=${apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -56,7 +56,7 @@ $(document).ready(function() {
 
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=47.6129432%2C-122.4821474%2C100&user_location=47.6129432%2C-122.4821474&skip=0&limit=25&user_key=${apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=47.6129432%2C-122.4821474%2C25&user_location=47.6129432%2C-122.4821474&skip=0&limit=25&user_key=${apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
