eb9ae831cc592aa0d196f0a1009e0b383da025e4
Charlie Kelson <charlie.m.kelson@gmail.com>
2017-09-15 10:19:43 -0700

    test API calls.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,23 +1,86 @@
-import { Doctor } from './../js/doctor.js';
+var apiKey = require('./../.env').apiKey;
 
-$(function() {
-  $('#med-issue-form').submit(function(event) {
+$(document).ready(function() {
+  $('#med-issue-form').submit(function() {
     event.preventDefault();
-    var medIssue = $('#med-issue').val();
-    var doctor = new Doctor();
-    var output = doctor.findMedIssue(medIssue);
-    output.forEach(function(doctor) {
-      $('#solution').append("<li>" + element + "</li>");
+      let medIssue = $('#med-issue').val();
+
+      $('#med-issue').val("");
+
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${medIssue}&location=47.6129432%2C-122.4821474%2C100&user_location=47.6129432%2C-122.4821474&skip=0&limit=25&user_key=${apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      console.log(body);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
     });
   });
 
-  $('#find-doc-form').submit(function(event) {
-    event.preventDefault();
-    var docName = $('#doc-name').val();
-    var doctor = new Doctor();
-    var output = doctor.findDoctor(name);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
+  $(document).ready(function() {
+    $('#doc-name-form').submit(function() {
+      event.preventDefault();
+        let docName = $('#doc-name').val();
+
+        $('#doc-name').val("");
+
+      let promise = new Promise(function(resolve, reject) {
+        let request = new XMLHttpRequest();
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=47.6129432%2C-122.4821474%2C100&user_location=47.6129432%2C-122.4821474&skip=0&limit=25&user_key=${apiKey}`;
+        request.onload = function() {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(Error(request.statusText));
+          }
+        }
+        request.open("GET", url, true);
+        request.send();
+      });
+
+      promise.then(function(response) {
+        let body = JSON.parse(response);
+        console.log(body);
+      }, function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      });
+      });
     });
-  });
-});
+
+// $(function() {
+//   $('#med-issue-form').submit(function(event) {
+//     event.preventDefault();
+//     var medIssue = $('#med-issue').val();
+//     var doctor = new Doctor();
+//     var output = doctor.test(medIssue);
+//     console.log(doctor.test(medIssue));
+//     // output.forEach(function(doctor) {
+//     //   $('#solution').append("<li>" + element + "</li>");
+//     // });
+//   });
+//   });
+// $(function() {
+//   $('#find-doc-form').submit(function(event) {
+//     event.preventDefault();
+//     var docName = $('#doc-name').val();
+//     var doctor = new Doctor();
+//     var output = doctor.findDoctor(name);
+//     output.forEach(function(element) {
+//       $('#solution2').append("<li>" + element + "</li>");
+//     });
+//   });
+//
+// });
